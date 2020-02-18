1fdcfe6a50f86ce6c637a2cd6905714558c5e4fb
Paulo <galuzio.paulo@protonmail.com>
2020-02-18 11:39:04 -0500

    Analysis files

---
+++ Analisa.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as pl
+import pandas as pd
+
+M = pd.read_csv("M.dat",
+                sep=' ',
+                names=["NDim",  # Dimensionalidade do espaço de busca
+                       "Iter",  # Número de avaliações da função objetivo
+                       "N_init",  # Pontos iniciais
+                       "NPF",     # N. de pts na PF
+                       "GD",      # Generational distance
+                       "SS",      # Spread
+                       "HV",      # Hyervolume
+                       "HausDist",  # Haussdorf Distance
+                       "Cover",     # Coverage
+                       "GDPS",      # GD measured by PS
+                       "SSPS",      # Spread measured by PS
+                       "HDPS",  # Haussdorf distance measured by PS
+                       "Prob",  # Probability
+                       "q",     # Q
+                       "Front"])  # Front Filename
+
+ZG = {}
+Keys = []
+
+Metric = "GD"
+
+fig = pl.figure(Metric)
+
+ZG = zdt1.\
+    groupby("NPF").get_group(100).\
+    groupby("Iter").agg(np.mean).reset_index()
+
+pl.plot(ZG["Iter"], ZG[Metric], 'o')
+print(ZG[Metric])
+
+pl.legend()
+pl.grid()
+pl.xscale("log")
+pl.yscale("log")
+fig.show()
---
+++ PrintFront.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Simple script that prints the Pareto front of a given file
+"""
+
+import numpy as np
+import matplotlib.pyplot as pl
+from scipy.spatial.distance import directed_hausdorff as HD
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", dest="Filename", type=str,
+                    help="Filename with front")
+
+parser.add_argument("--tpf", dest="TPF", type=str,
+                    help="true Pareto front", default=None,
+                    required=False)
+
+args = parser.parse_args()
+
+if args.TPF is not None:
+    F1, F2 = np.loadtxt(args.TPF, unpack=True)
+    ISorted = np.argsort(F1)
+    f1 = F1[ISorted]
+    f2 = F2[ISorted]
+
+if args.Filename[-3:] == "dat" or args.Filename[-3:] == "txt":
+
+    F1D2, F2D2 = np.loadtxt(args.Filename, unpack=True)
+    I2 = np.argsort(F1D2)
+
+elif args.Filename[-3:] == "npz":
+    Data = np.load(args.Filename)
+    F1D2, F2D2 = Data["Front"][:, 0], Data["Front"][:, 1]
+    I2 = np.argsort(F1D2)
+
+else:
+    raise TypeError("Extension should be txt, dat or npz")
+
+pl.plot(F1D2[I2], F2D2[I2], 'o--', label="Bayes")
+if args.TPF is not None:
+    pl.plot(f1, f2, 'o', label="TPF")
+    pl.legend()
+
+    print("Antes")
+    print(HD(Data["Front"], np.asarray([f1, f2]).T)[0])
+pl.xlabel(r"$f_1$")
+pl.ylabel(r"$f_2$")
+
+pl.grid()
+
+pl.show()
