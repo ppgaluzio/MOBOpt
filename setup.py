bdd3a8aa69dd01da5668d43db2227edc3e0b880c
Paulo <galuzio.paulo@protonmail.com>
2020-02-20 18:29:34 -0500

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MOBOpt",
-    version="0.0.2",
+    version="0.0.3",
     author="P. P. Galuzio",
     author_email="galuzio.paulo@protonmail.com",
     description="Multi-Objective Bayesian Optimization",
