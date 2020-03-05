388ec84311c75dcb52eea997d7ba422350757552
Paulo <galuzio.paulo@protonmail.com>
2020-03-04 19:48:38 -0500

    Updated url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Multi-Objective Bayesian Optimization",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ppgaluzio/PastaPhase",
+    url="https://github.com/ppgaluzio/MOBOpt",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
