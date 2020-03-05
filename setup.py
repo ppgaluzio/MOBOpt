c22f39a3bdf765275755e7a378d03c11a1c7c779
Paulo <galuzio.paulo@protonmail.com>
2020-03-05 09:29:08 -0500

    Updated license in setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPL-3.0 License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
