2cc0981e99d8d4c8405fc3e5e5306dd7b36d5c2d
Paulo <galuzio.paulo@protonmail.com>
2020-07-13 17:43:12 -0400

    DEV: KPLS wrapper

--- mobopt/_wrapper.py
+++ mobopt/_wrapper.py
@@ -1,5 +1,19 @@
 from sklearn.gaussian_process import GaussianProcessRegressor
+# from smt.surrogate_models import KPLS
 
 
 class GaussianProcessWrapper(GaussianProcessRegressor):
     pass
+
+
+# class GaussianProcessWrapper:
+#     def __init__(self, *args, **kwargs):
+#         self.sm = KPLS(theta0=[1e-2], print_global=False)
+#         print("KPLS method")
+
+#     def fit(self, X, y):
+#         self.sm.set_training_values(X, y)
+#         self.sm.train()
+
+#     def predict(self, X):
+#         return self.sm.predict_values(X).ravel()
